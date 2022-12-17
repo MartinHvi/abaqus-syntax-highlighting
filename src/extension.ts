2ae5d78c3f1ac770dcd34e10ef8cfd74233149ce
MartinHvi <77893112+MartinHvi@users.noreply.github.com>
2022-12-17 15:25:15 +0100

    Added commands to indent, remove leading spaces and format

--- src/extension.ts
+++ src/extension.ts
@@ -39,5 +39,98 @@ export function activate(context: vscode.ExtensionContext) {
 			};
 		}
 	});
-	context.subscriptions.push(hoverProvider);
+
+	const disposable_indentLines = vscode.commands.registerCommand('extension.indentLines', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return; // No open text editor
+		}
+
+		const document = editor.document;
+		const lineCount = document.lineCount;
+
+		editor.edit(editBuilder => {
+			for (let i = 0; i < lineCount; i++) {
+				const line = document.lineAt(i);
+				const lineText = line.text;
+				if (!lineText.startsWith('*') && !lineText.startsWith(' ')) {
+					editBuilder.insert(line.range.start, ' ');
+				}
+			}
+		});
+	});
+
+	const disposable_removeLeadingSpaces = vscode.commands.registerCommand('extension.removeLeadingSpaces', () => {
+		// Get the current text editor
+		let editor = vscode.window.activeTextEditor;
+
+		if (!editor) {
+			return;
+		}
+
+		// Get the range of the entire document
+		let documentRange = new vscode.Range(0, 0, editor.document.lineCount - 1, editor.document.lineAt(editor.document.lineCount - 1).text.length);
+
+		// Get the text of the document
+		let documentText = editor.document.getText(documentRange);
+
+		// Split the document into lines
+		let lines = documentText.split('\n');
+
+		// Initialize an array for the modified lines
+		let modifiedLines = [];
+
+		// Iterate over the lines
+		for (let i = 0; i < lines.length; i++) {
+			if (lines[i][0] !== '*') {
+				// If the line does not start with an asterisk, remove all leading spaces
+				modifiedLines.push(lines[i].replace(/^\s+/, ''));
+			} else {
+				// If the line starts with an asterisk, leave it unchanged
+				modifiedLines.push(lines[i]);
+			}
+		}
+
+		// Join the modified lines into a single string
+		let modifiedText = modifiedLines.join('\n');
+
+		// Replace the document text with the modified text
+		editor.edit(editBuilder => {
+			editBuilder.replace(documentRange, modifiedText);
+		});
+	});
+
+	const disposable_format = vscode.commands.registerCommand('extension.format', () => {
+		// Get the current text editor
+		let editor = vscode.window.activeTextEditor;
+
+		if (!editor) {
+			return;
+		}
+		// Get the range of the entire document
+		let documentRange = new vscode.Range(0, 0, editor.document.lineCount - 1, editor.document.lineAt(editor.document.lineCount - 1).text.length);
+		// Get the text of the document
+		let documentText = editor.document.getText(documentRange);
+		// Split the document into lines
+		let lines = documentText.split('\n');
+		// Initialize an array for the modified lines
+		let modifiedLines = [];
+		// Iterate over the lines
+		for (let i = 0; i < lines.length; i++) {
+			// Remove spaces after commas and add a single space after all commas
+			modifiedLines.push(lines[i].replace(/,\s+/g, ',').replace(/,/g, ', '));
+		}
+		// Join the modified lines into a single string
+		let modifiedText = modifiedLines.join('\n');
+		// Replace the document text with the modified text
+		editor.edit(editBuilder => {
+			editBuilder.replace(documentRange, modifiedText);
+		});
+	});
+
+	// Finally dispose
+	context.subscriptions.push(hoverProvider, disposable_indentLines, disposable_removeLeadingSpaces, disposable_format);
 }
+
+
+//export function deactivate() { }
