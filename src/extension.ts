72eff74893e01714ea8c6e135511d37ac896e9fb
MartinHvi <77893112+MartinHvi@users.noreply.github.com>
2022-12-17 22:12:21 +0100

    Removed comments

--- src/extension.ts
+++ src/extension.ts
@@ -15,29 +15,6 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposableRemoveAllComments = vscode.commands.registerCommand('extension.removeAllComments', removeAllComments);
 	const disposableRemoveEmptyComments = vscode.commands.registerCommand('extension.removeEmptyComments', removeEmptyComments);
 	const disposableRemoveBlankLines = vscode.commands.registerCommand('extension.removeBlankLines', removeBlankLines);
-
-
-	/* 	let disposable = vscode.commands.registerTextEditorCommand('extension.removeEmptyComments', (textEditor: vscode.TextEditor) => {
-			let document = textEditor.document;
-			let selection = textEditor.selection;
-	
-			let start = new vscode.Position(0, 0);
-			let end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
-			let range = new vscode.Range(start, end);
-	
-			let text = document.getText(range);
-			let lines = text.split('\n');
-	
-			let newLines = lines.filter(line => {
-				return !(line.trim() === '**' || line.trim() === '');
-			});
-	
-			let newText = newLines.join('\n');
-			textEditor.edit(editBuilder => {
-				editBuilder.replace(range, newText);
-			});
-		}); */
-
 	// Dispose
 	context.subscriptions.push(disposableHoverProvider, disposableIndentLines, disposableRemoveLeadingSpaces, disposableFormat, disposableCompact, disposableRemoveAllComments, disposableRemoveEmptyComments, disposableRemoveBlankLines);
 }
